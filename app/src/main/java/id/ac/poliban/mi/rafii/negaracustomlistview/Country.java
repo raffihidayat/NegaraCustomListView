392dbcc8204fa0cb224e9599f34ec800bee8fa77
raffihidayat <rafizhe@gmail.com>
2020-01-17 00:39:07 +0800

    Initial commit

---
+++ app/src/main/java/id/ac/poliban/mi/rafii/negaracustomlistview/Country.java
@@ -0,0 +1,43 @@
+package id.ac.poliban.mi.rafii.negaracustomlistview;
+
+public class Country {
+    private String flag;
+    private String countryName;
+    private String getCountryDetail;
+
+    public Country(String flag, String countryName, String getCountryDetail) {
+        this.flag = flag;
+        this.countryName = countryName;
+        this.getCountryDetail = getCountryDetail;
+    }
+
+    public String getFlag() {
+        return flag;
+    }
+
+    public void setFlag(String flag) {
+        this.flag = flag;
+    }
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getGetCountryDetail() {
+        return getCountryDetail;
+    }
+
+    public void setGetCountryDetail(String getCountryDetail) {
+        this.getCountryDetail = getCountryDetail;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%30s", getCountryName());
+    }
+}
+
